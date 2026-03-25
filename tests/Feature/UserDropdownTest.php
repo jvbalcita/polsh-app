507bfc41db45d340dc52d8521dec515abb890840
Jack Vincent Balcita <jack.balcita@hotmail.com>
2026-03-25 20:47:26 +0800

    style: use expect() consistently in UserDropdownTest

--- tests/Feature/UserDropdownTest.php
+++ tests/Feature/UserDropdownTest.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 it('teams.settings named route resolves to the expected URL', function () {
-    $this->assertTrue(
-        Route::has('teams.settings'),
-        'teams.settings route must exist for the My Team dropdown link'
-    );
+    expect(Route::has('teams.settings'))->toBeTrue('teams.settings route must exist for the My Team dropdown link');
 
     $url = route('teams.settings');
 
